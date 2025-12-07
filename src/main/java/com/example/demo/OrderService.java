8b4bc8ba22d2eb8e3f418db9ea546620e7bb9ecf
Rishes-Mishra <138433723+Rishes-Mishra@users.noreply.github.com>
2025-12-07 22:24:15 +0530

    used dependency injection

---
+++ src/main/java/com/example/demo/OrderService.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class OrderService {
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+
+    public void makePayment(){
+        paymentService.doPayment();
+    }
+
+}
